5296888cd71f512cf498344b887c5b4d7d1bd55d
bp96 <brijeshpatel96@gmail.com>
2020-01-02 15:44:02 +0000

    Updates

---
+++ precache-manifest.1f76a1c0755375293500523bfd1c3d1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c396ca42caf123a9bf027c9e5cddf3a4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dc95c0294105b863491b",
+    "url": "/robofriends/static/css/main.0a2805e9.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc95c0294105b863491b",
+    "url": "/robofriends/static/js/main.5405fb91.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
