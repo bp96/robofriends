24cbcf9530a9194c43750c8fa4d43ce260020d3a
bp96 <brijeshpatel96@gmail.com>
2021-05-20 18:11:42 +0100

    adding tachyon comments to Card JS file

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Card = (props) => {
 	const {name,email,id} = props;
 	return (
+		// classes using tachyons - align text in centre, light green background, display inline block, border 3*0.25rem, margin 2*0.25rem, grow animation, border width 2*0.25rem, shadow 5*0.25rem
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='robots' src= {`https://robohash.org/${id}200x200`} />
 			<div>
