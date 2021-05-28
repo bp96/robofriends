cb3514b6e58f29cf3709f2c650523ade70e25354
bp96 <brijeshpatel96@gmail.com>
2021-05-28 19:08:21 +0100

    altering robothash url

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
 	return (
 		// classes using tachyons - align text in centre, light green background, display inline block, border 3*0.25rem, margin 2*0.25rem, grow animation, border width 2*0.25rem, shadow 5*0.25rem
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src= {`https://robohash.org/${id}200x200`} />
+			<img alt='robots' src= {`https://robohash.org/${id}?size=200x200`} />
 			<div>
 				<h2> {name} </h2>	
 				<p> {email} </p>
